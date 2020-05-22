4b229280fe0a9a74c4e4cfcbb06d97babfd6fa39
ThisThato <iamkamzar@gmail.com>
2020-05-22 23:01:51 +0200

    Github repo route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 const auth = require("../../middleware/auth");
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
 const { check, validationResult } = require("express-validator");
+const request = require("request");
+const config = require("config");
 
 //GET api/profile/me
 //Get current users profile
@@ -349,6 +352,29 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
   } catch (err) {
     console.error(err.message);
     res.status(500).json("Server Error");
+  }
+});
+
+//GET  api/profile/github/:username
+//GET  user profiles from github
+//Public route
+router.get("/github/:username", async (req, res) => {
+  try {
+    const uri = encodeURI(
+      `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+    );
+
+    const headers = {
+      "user-agent": "node.js",
+      Authorization: `token ${config.get("githubToken")}`,
+    };
+
+    const githubResponse = await axios.get(uri, { headers });
+
+    return res.json(githubResponse.data);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(404).json({ msg: "No Github profile found" });
   }
 });
 
